ba519d2f7b839cf9af9380eaf715c5ee8e1e604a
KumarSaroj99 <sarojkumar.panda@aurionpro.com>
2024-07-30 10:37:26 +0530

    yash

---
+++ TicTacToe/Program.cs
@@ -0,0 +1,111 @@
+﻿namespace TicTacToe
+{
+    internal class Program
+    {
+        static char[] board = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+        static char currentPlayer = 'X';
+
+        static void Main()
+        {
+            int move;
+            bool gameWon = false;
+
+            do
+            {
+                Console.Clear();
+                DisplayBoard();
+                Console.WriteLine($"Player {currentPlayer}, enter your move (1-9): ");
+                move = int.Parse(Console.ReadLine());
+
+                if (MoveIsValid(move))
+                {
+                    MakeMove(move);
+                    gameWon = CheckWin();
+                    if (!gameWon)
+                    {
+                        ChangePlayer();
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid move! Press any key to try again...");
+                    Console.ReadKey();
+                }
+            } while (!gameWon && !IsBoardFull());
+
+            Console.Clear();
+            DisplayBoard();
+
+            if (gameWon)
+            {
+                Console.WriteLine($"Player {currentPlayer} wins!");
+            }
+            else
+            {
+                Console.WriteLine("It's a draw!");
+            }
+        }
+
+        static void DisplayBoard()
+        {
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine($"  {board[0]}  |  {board[1]}  |  {board[2]}  ");
+            Console.WriteLine("  ___|_____|___");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine($"  {board[3]}  |  {board[4]}  |  {board[5]}  ");
+            Console.WriteLine("  ___|_____|___");
+            Console.WriteLine("     |     |     ");
+            Console.WriteLine($"  {board[6]}  |  {board[7]}  |  {board[8]}  ");
+            Console.WriteLine("     |     |     ");
+        }
+
+        static bool MoveIsValid(int move)
+        {
+            return move >= 1 && move <= 9 && board[move - 1] != 'X' && board[move - 1] != 'O';
+        }
+
+        static void MakeMove(int move)
+        {
+            board[move - 1] = currentPlayer;
+        }
+
+        static void ChangePlayer()
+        {
+            currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        }
+
+        static bool CheckWin()
+        {
+            int[,] winConditions = new int[,]
+            {
+            {0, 1, 2}, {3, 4, 5}, {6, 7, 8}, // Rows
+            {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, // Columns
+            {0, 4, 8}, {2, 4, 6}             // Diagonals
+            };
+
+            for (int i = 0; i < winConditions.GetLength(0); i++)
+            {
+                if (board[winConditions[i, 0]] == currentPlayer &&
+                    board[winConditions[i, 1]] == currentPlayer &&
+                    board[winConditions[i, 2]] == currentPlayer)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        static bool IsBoardFull()
+        {
+            foreach (char spot in board)
+            {
+                if (spot != 'X' && spot != 'O')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
